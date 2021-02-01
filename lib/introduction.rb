9ade83df6c9754548b10183a390d94ee2c745734
Susana vik <susana.m.vik@gmail.com>
2021-02-01 15:34:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name} and I am excited to lean #{language}!"
 end
 
 name = "Susana"
